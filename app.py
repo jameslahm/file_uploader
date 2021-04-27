3fa31c132abf3c1d39fa7dfd4c33c69085173ea4
jameslahm <1366463855@qq.com>
2021-04-27 19:09:37 +0800

    feat: init

---
+++ app.py
@@ -0,0 +1,43 @@
+import uuid
+from flask import Flask,request,jsonify
+from flask.globals import current_app
+from flask.helpers import send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SECRET = "jcnabckydusja"
+UPLOAD_DIR = os.path.join(BASE_DIR,"static")
+API_BASE = "http://139.196.81.14"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    token = request.headers.get("Authorization","")
+
+    if token!=SECRET:
+        return jsonify({"error":"bad request"}),401
+
+    if 'file' not in request.files:
+        return jsonify({"error":"no file"}),400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error":"no file"}),400
+    if file :
+        if not os.path.exists(UPLOAD_DIR):
+            os.makedirs(UPLOAD_DIR)
+        filename = secure_filename(file.filename)
+        random_string  = uuid.uuid4().hex
+        filename = random_string + filename 
+        
+        file.save(os.path.join(UPLOAD_DIR, filename))
+        return jsonify({"url":API_BASE + '/upload/'+filename}),200
+
+@app.route('/upload/<filename>',methods=['GET'])
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_DIR,filename)
+
+if __name__=="__main__":
+    app.run("0.0.0.0",port=7998)
